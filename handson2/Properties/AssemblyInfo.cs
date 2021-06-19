9846ae37d38f3eb68ca0526ec9192a5c888d771d
J <910455@cognizant.com>
2021-06-19 15:20:15 +0530

    Hi

---
+++ handson2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("handson2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("handson2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61bf5bfd-7428-4654-b979-3c9d3b2d6b67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ handson2/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+using NUnit.Framework;
+
+namespace handson2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+    
+        [TestCase(20,10,10)]
+        [TestCase(10,20,-10)]
+        [TestCase(10,10,0)]
+        [TestCase(10,0,10)]
+        public void Subtraction(double a,double b,double c)
+        {
+            SimpleCalculator sc = new SimpleCalculator();
+            double result = sc.Subtraction(a, b);
+            Assert.AreEqual(c, result);
+
+        }
+        [TestCase(20, 10, 200)]
+        [TestCase(10, -20, -200)]
+        [TestCase(-10, 20, -200)]
+        [TestCase(10, 20, 200)]
+        [TestCase(10, 0, 0)]
+        [TestCase(0, 20, 0)]
+
+        public void Multiplication(double a, double b, double c)
+        {
+            SimpleCalculator sc = new SimpleCalculator();
+            double result = sc.Multiplication(a, b);
+            Assert.AreEqual(c, result);
+
+        }
+        [TestCase(20, 10, 2)]
+        [TestCase(10, 20, 0.5)]
+        [TestCase(0, 20, 0)]
+        public void Division(double a, double b, double c)
+        {
+            SimpleCalculator sc = new SimpleCalculator();
+            double result = sc.Division(a, b);
+            if (b == 0)
+            {
+                Assert.Fail("Division by 0");
+            }
+            Assert.AreEqual(c, result);
+        }
+    }
+}
